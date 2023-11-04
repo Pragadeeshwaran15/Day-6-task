90377a868458f2f88285dab986bd9c2803827549
Pragadeeshwaran15 <pragadeshm501@gmail.com>
2023-11-04 11:04:27 +0530

    Completed

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-// //Q.1
-// //Answer:
-// class movie{
-//     constructor(title,studio,rating){
-//         this.title=title;
-//         this.studio=studio;
-//         this.rating=rating;
-//     }
-//     set name
-// }
+//Q.1
+//Answer:
+class movie{
+    constructor(title,studio,rating="PG"){
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
+    set name(n){
+          this._title=n;
+    }
+    get name(){
+        return this.Title;
+    }
+    set studio(x){
+        this.Studio=x;
+  }
+  get studio(){
+      return this.Studio;
+  }
+    set rating(r){
+        this.Rating=r;
+  }
+  get rating(){
+      return this.Rating;
+  }
+
+}
+var mov=new movie("Casino Royale","Eon Productions","PG13")
+var mov1=new movie("Casino Royale","Eon Productions")
+console.log(mov)
+console.log(mov1)
+
+
+
+
 //Q.2
 //Answer:
 class Circle{
@@ -49,7 +75,6 @@ console.log(object.toString)
 console.log(object.areaCricle)
 console.log(object.circumferenceCricle)
 
-const result=new movie("Casino Royale", "Eon Productions","PG­13")
 //Q.3 Write a “person” class to hold all the details.
 //Answer:
 class person{
@@ -66,4 +91,21 @@ console.log(`The name of the person is ${person1.name}
 Age of the person is ${person1.age}
 Gender ${person1.gender}
 Experience ofthe person is ${person1.experience}`)
+
+//Q.4 write a class to calculate the uber price.
+//Answer:
+class uberPrice{
+    constructor(km,amount=20){
+          this.km=km;
+          this.amount=amount;
+    }
+    get price(){
+     return this.km*this.amount;
+    }
+
+}
+var value1=new uberPrice(8)
+var value2=new uberPrice(10,35)
+console.log(value1.price)
+console.log(value2.price)
 
